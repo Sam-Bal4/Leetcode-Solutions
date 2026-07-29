3a0571786d9b785b1d315db77177cee5cb06912a
Samuel T Balfour <samuelbalfour34@gmail.com>
2026-07-29 17:52:12 -0600

    File updated at 7/29/2026, 5:52:12 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,24 +1,18 @@
-// Last updated: 7/29/2026, 5:35:57 PM
+// Last updated: 7/29/2026, 5:52:12 PM
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
-3
-4        int[] indices = new int[2];
-5        int sum;
-6
-7        for(int i = 0; i < nums.length; i++){
-8
-9                for(int j = i+1; j < nums.length; j++){
-10                    sum = nums[i] + nums[j];
-11                    if(sum == target){
-12                        indices[0] = i;
-13                        indices[1] = j;
-14                        break;
-15                    }
-16                }
-17                if(indices[1] != 0){
-18                    break;
-19                }
-20            }
-21        return indices;
-22    }
-23}
+3        int valueNeeded;
+4        int[] index = new int[2];
+5        HashMap<Integer, Integer> indices = new HashMap<>();
+6        for(int i = 0; i < nums.length; i++){
+7            valueNeeded = target - nums[i];
+8            if(indices.get(valueNeeded) != null){
+9                index[0] = i;
+10                index[1] = indices.get(valueNeeded);
+11            }else{
+12                indices.put(nums[i], i);
+13            }
+14        }
+15        return index;
+16    }
+17}
