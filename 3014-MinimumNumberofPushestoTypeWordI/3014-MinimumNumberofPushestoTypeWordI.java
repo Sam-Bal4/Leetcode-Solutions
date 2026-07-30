e9401861d7f2548514ae34a89064ece331fe2185
Samuel T Balfour <samuelbalfour34@gmail.com>
2026-07-30 11:16:08 -0600

    File updated at 7/30/2026, 11:16:07 AM

--- 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.java
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.java
@@ -1,36 +1,47 @@
-// Last updated: 7/30/2026, 11:13:08 AM
+// Last updated: 7/30/2026, 11:16:07 AM
 1class Solution {
 2    public int minimumPushes(String word) {
 3        int minPushes = 0;
 4        int count = 1;
 5        int wordSize = word.length();
-6        if (wordSize <= 8) {
-7            return wordSize;
-8        } else {
-9            wordSize -= 8;
-10            minPushes += 8 * count;
-11            count++;
-12            if (wordSize <= 8) {
-13                minPushes += wordSize * count;
-14            } else {
-15                wordSize -= 8;
-16                minPushes += 8 * count;
-17                count++;
-18                if (wordSize <= 8) {
-19                    minPushes += wordSize * count;
-20                } else {
-21                    wordSize -= 8;
-22                    minPushes += 8 * count;
-23                    count++;
-24                    if (wordSize <= 8) {
-25                        minPushes += wordSize * count;
-26                    } else {
-27                        wordSize -= 8;
-28                        minPushes += 8 * count;
-29                    }
-30                }
-31            }
-32        }
-33        return minPushes;
-34    }
-35}
+6
+7        //Done using if statements
+8        /*
+9        if (wordSize <= 8) {
+10            return wordSize;
+11        } else {
+12            wordSize -= 8;
+13            minPushes += 8 * count;
+14            count++;
+15            if (wordSize <= 8) {
+16                minPushes += wordSize * count;
+17            } else {
+18                wordSize -= 8;
+19                minPushes += 8 * count;
+20                count++;
+21                if (wordSize <= 8) {
+22                    minPushes += wordSize * count;
+23                } else {
+24                    wordSize -= 8;
+25                    minPushes += 8 * count;
+26                    count++;
+27                    if (wordSize <= 8) {
+28                        minPushes += wordSize * count;
+29                    } else {
+30                        wordSize -= 8;
+31                        minPushes += 8 * count;
+32                    }
+33                }
+34            }
+35        }*/
+36
+37        //Done using while loop
+38        while(wordSize > 8){
+39            wordSize -= 8;
+40            minPushes += 8 * count;
+41            count++;
+42        }
+43        minPushes += wordSize * count;
+44        return minPushes;
+45    }
+46}
