a66dba998cf6f3b647f4f2b851178a0056cc8404
Samuel T Balfour <samuelbalfour34@gmail.com>
2026-07-31 16:52:50 -0600

    File updated at 7/31/2026, 4:52:50 PM

--- 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.java
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.java
@@ -1,15 +1,25 @@
-// Last updated: 7/30/2026, 11:16:28 AM
+// Last updated: 7/31/2026, 4:52:50 PM
 1class Solution {
 2    public int minimumPushes(String word) {
-3        int minPushes = 0;
-4        int count = 1;
-5        int wordSize = word.length();
-6        while(wordSize > 8){
-7            wordSize -= 8;
-8            minPushes += 8 * count;
-9            count++;
-10        }
-11        minPushes += wordSize * count;
-12        return minPushes;
-13    }
-14}
+3        int[] freq = new int[26];
+4
+5        for (char c : word.toCharArray()) {
+6            freq[c - 'a']++;
+7        }
+8
+9        Arrays.sort(freq);
+10
+11        int minPushes = 0;
+12        int count = 0;
+13
+14        for (int i = 25; i >= 0; i--) {
+15            if (freq[i] == 0) break; 
+16            int multiplier = (count / 8) + 1;
+17            minPushes += freq[i] * multiplier;
+18            
+19            count++;
+20        }
+21
+22        return minPushes;
+23    }
+24}
