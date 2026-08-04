7d68f54e60edc9234b37504932a4b797938ae7de
Samuel T Balfour <samuelbalfour34@gmail.com>
2026-08-04 15:41:49 -0600

    File updated at 8/4/2026, 3:41:49 PM

--- 3731-FindMissingElements/3731-FindMissingElements.java
+++ 3731-FindMissingElements/3731-FindMissingElements.java
@@ -1,22 +1,20 @@
-// Last updated: 8/4/2026, 3:41:30 PM
+// Last updated: 8/4/2026, 3:41:49 PM
 1class Solution {
 2    public List<Integer> findMissingElements(int[] nums) {
 3        Arrays.sort(nums);
-4        //int missing = nums[nums.length-1] - nums.length;
-5        //int[] missingValues = new int[missing];
-6        ArrayList<Integer> missingValues = new ArrayList<Integer>();
-7
-8        for(int i = 1; i < nums.length; i++){
-9            int currentValue = nums[i] - 1;
-10            int previousValue = nums[i-1];
-11
-12            int count;
-13            while(currentValue != previousValue){
-14                missingValues.add(currentValue);
-15                currentValue--;
-16            }
-17        }
-18        missingValues.sort(null);
-19        return missingValues;
-20    }
-21}
+4        ArrayList<Integer> missingValues = new ArrayList<Integer>();
+5
+6        for(int i = 1; i < nums.length; i++){
+7            int currentValue = nums[i] - 1;
+8            int previousValue = nums[i-1];
+9
+10            int count;
+11            while(currentValue != previousValue){
+12                missingValues.add(currentValue);
+13                currentValue--;
+14            }
+15        }
+16        missingValues.sort(null);
+17        return missingValues;
+18    }
+19}
